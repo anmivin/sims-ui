bb2cf81c1ab08d93d828801661cc91420594be17
Nastya Feeva <anmivin@yandex.ru>
2024-08-12 22:41:54 +0300

    commit1

---
+++ lib/colors.ts
@@ -0,0 +1,21 @@
+export const colors = {
+  sims4: {
+    buttonGradient: "linear-gradient(180deg, #fbfbfb 20%, #d9d9d9)",
+    darkBlue: "#0949ab",
+    lightBlue: "#1e81e0",
+    buttonShadow: "0 4px 6px 0 #606164",
+    checkbosShadow: "0 4px 6px 0 #848484",
+    green: "#199c2c",
+    white: "#fff",
+    modal: "rgba(251, 251, 251, 0.7)",
+    modalShadow: "0px 0px 9px 1px rgba(0, 0, 0, 0.3)",
+    checkboxChecked: " linear-gradient(180deg, #5db823 40%, #269331);",
+    checkboxUnchecked: "linear-gradient(180deg, #f9f9f9 40%, #dddddd)",
+    selectedButton: "linear-gradient(180deg, #92ce31 20%, #3bb435)",
+  },
+  sims2: {
+    dark: "#121B61",
+    medium: "#95A6DE",
+    light: "#CDD6FF",
+  },
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,58 @@
-
-import {Button, Button2, Checkbox, Checkbox2, TextField, TextField2, Modal2, Modal, IconButton, MenuButton} from 'sims-ui'
-import '../lib/fonts/simsSans.css'
-import {CloxeIcon} from '../lib/icons/closeIcon'
-import CloseIcon from '../lib/icons/CloseIcon copy'
+import {
+  Button,
+  Button2,
+  Checkbox,
+  Checkbox2,
+  TextField,
+  TextField2,
+  Modal2,
+  Modal,
+  IconButton,
+  MenuButton,
+} from "sims-ui";
+import "../lib/fonts/simsSans.css";
+import { CloxeIcon } from "../lib/icons/closeIcon";
+import CloseIcon from "../lib/icons/CloseIcon copy";
+import './App.css';
 function App() {
-
   return (
     <>
-      <div style={{backgroundColor: '#c8d5e6', padding: '60px', display:'flex', flexWrap: 'wrap', gap: 8}}>
-{/*        
-       <Button>dsfsdf</Button>
-       <Button2>dsfsdf</Button2>
-       <Checkbox label='sadasdasdaasda'/>
-       <Checkbox2 label='sadasdasdaasda'/>
+          <div
+        style={{
+          backgroundColor: "#c8d5e6",
+          padding: "60px",
+width: '100px',
+height: '100px',
+overflowY: 'auto'
+
+        }}
+      >"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</div>
+      <div
+        style={{
+          backgroundColor: "#c8d5e6",
+          padding: "60px",
+          display: "flex",
+          flexWrap: "wrap",
+          gap: 8,
+        }}
+      >
+        {/*         <Button>КНОпка</Button>
+        <Button2>КНОПка</Button2> */}
+        <Checkbox label='sadasdasdaasda' />
+        {/*    <Checkbox2 label='sadasdasdaasda'/>
        <TextField placeholder='sda'/>
        <TextField2 placeholder='sda'/> */}
-{/*        <Modal2/> */}
-       {/*  */}
-       <MenuButton>sdasdasdasd</MenuButton>
-       <MenuButton selected>sdasdasdasd</MenuButton>
-       <IconButton><CloseIcon size={40}/></IconButton>
-       <Modal title='sadlaskdlkaskd'>saddsadasdasda asda sda asdasdas asdas</Modal>
+        {/*        <Modal2/> */}
+        {/*  */}
+        {/*         <MenuButton>sdasdasdasd</MenuButton>
+        <MenuButton selected>sdasdasdasd</MenuButton>
+        <IconButton>
+          <CloseIcon size={40} />
+        </IconButton> */}
+        <Modal title='sadlaskdlkaskd'>saddsadasdasda asda sda asdasdas asdas</Modal>
       </div>
-
     </>
-  )
+  );
 }
 
-export default App
+export default App;
